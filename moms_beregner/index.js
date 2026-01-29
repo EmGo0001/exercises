b973a2fa09a4900e2449d981602b934b5ea4e55c
Emily <emgo0001@stud.kea.dk>
2026-01-29 15:03:26 +0100

    moms beregner opdateret

--- moms_beregner/index.js
+++ moms_beregner/index.js
@@ -1,10 +1,16 @@
 "use stict";
 
-let Price = 30;
-let Moms = 1.25;
+// let Price = 30;
+// let Moms = 1.25;
 
-function fullPrice(){
-    console.log(Price*Moms)
+// function fullPrice(){
+//     console.log(Price*Moms)
+// }
+
+// fullPrice(Price,Moms);
+
+fullPrice(200);
+
+function fullPrice(price, moms = 25){
+    console.log("full price", (price * moms) / 100 + price);
 }
-
-fullPrice(Price,Moms);
