be3c226201b642ca4cbb5d26f1dbcb1a45c1d3e2
Emily <emgo0001@stud.kea.dk>
2026-02-06 14:58:37 +0100

    language switcher

--- filter/script.js
+++ filter/script.js
@@ -11,20 +11,91 @@ const vehicles = [
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
 const tbodyPointer = document.querySelector("tbody");
-showTheseVehicles(vehicles);
+// showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
+  tbodyPointer.innerHTML = ""
   arr.forEach((each) => {
+    // let stopText;
+    // if(each.stop){
+    //   stopText =each.stops;
+    // }
+    // else{
+    //   stopText = each.stops;
+    // }
     tbodyPointer.innerHTML += `<tr>
   <td>${each.type}</td>
   <td>${each.fuel}</td>
   <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
+  <td>${each.stops ?? "-"}</td>
   <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
+  <td>${each.isElectric ? "X": "-"}</td>
+  <td>${each.isTandem ? "X": "-"}</td>
 </tr>`;
   });
 }
 
+// Alle el drevne fartøjer
+function isElectric(vehicles){
+  if(vehicles.isElectric === true){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
+const onlyElectric = vehicles.filter(isElectric);
+// showTheseVehicles(onlyElectric);
+
+// Alle fartøjer med mere end 2 sæder
+function checkSeats (vehicles){
+  if(vehicles.passengers > 2){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+const moreThanTwoSeats = vehicles.filter(checkSeats);
+// showTheseVehicles(moreThanTwoSeats);
+
+// Alle el-drevne fartøjer ejet af Jonas
+function electricJonas (vehicles){
+  if(vehicles.isElectric === true && vehicles.ownedBy === "Jonas" ){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+const electricOwnedByJonas = vehicles.filter(electricJonas);
+// showTheseVehicles(electricOwnedByJonas);
+
+// Alle rugbrøds drevne fartøjer med plads til mere end en
+function rugbrød (vehicles){
+  if(vehicles.fuel === "Rugbrød" && vehicles.passengers > 1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+const rugbrødMoreThanOne = vehicles.filter(rugbrød);
+// showTheseVehicles(rugbrødMoreThanOne);
+
+// 4 + 1 knap
+const electricBtn = document.querySelector(".filter1");
+const seatsBtn = document.querySelector(".filter2");
+const vehicleJonasBtn = document.querySelector(".filter3");
+const ryebreadBtn = document.querySelector(".filter4");
+const noFilterBtn = document.querySelector(".filter5");
+
+electricBtn.addEventListener("click", () => showTheseVehicles(onlyElectric));
+seatsBtn.addEventListener("click", () => showTheseVehicles(moreThanTwoSeats));
+vehicleJonasBtn.addEventListener("click", () => showTheseVehicles(electricOwnedByJonas));
+ryebreadBtn.addEventListener("click", () => showTheseVehicles(rugbrødMoreThanOne));
+noFilterBtn.addEventListener("click", () => showTheseVehicles(vehicles));
---
+++ language_switcher/js.js
@@ -0,0 +1,38 @@
+"use strict";
+const texts = {
+  de: {
+    texts: [
+      { text: "Das Bot", location: ".header" },
+      { text: "Das Ro-Bot", location: ".footer" },
+    ],
+  },
+  da: {
+    texts: [
+      { text: "Båden", location: ".header" },
+      { text: "Robotten", location: ".footer" },
+    ],
+  },
+};
+let locale = "da";
+console.log(texts.da.texts[0]);
+texts[locale].texts.forEach((elm) =>{
+  document.querySelector(elm.location).textContent = elm.text;
+});
+
+const dropdown = document.querySelector("#dropdown");
+
+dropdown.addEventListener("change", changeLanguage);
+function changeLanguage(){
+  if(dropdown.value === "de"){
+  let locale = "de";
+  texts[locale].texts.forEach((elm) =>{
+  document.querySelector(elm.location).textContent = elm.text;
+});
+  }
+  else{
+    let locale = "da";
+    texts[locale].texts.forEach((elm) =>{
+  document.querySelector(elm.location).textContent = elm.text;
+});
+  }
+}
